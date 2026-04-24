36ab5cf24366f6c61dcffd1d6f58279e2d78ae1f
kamikass <kassimjap@gmail.com>
2026-04-24 11:10:40 +0200

    message

--- mock/map_mocked.py
+++ mock/map_mocked.py
@@ -10,20 +10,20 @@ MAP_MOCKED = [
     [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
     [1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,1],
     [1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,1],
-    [1,1,0,1,1,0,1,1,0,1,1,0,1,1,0,1,1,0,1,1,1],
+    [1,1,0,1,1,0,1,1,0,1,1,0,1,1,0,1,1,0,1,0,1],
     [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
     [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1],
     [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,1],
-    [1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1],
-    [1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,1],
-    [1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1],
+    [1,0,0,1,0,0,0,1,1,1,1,1,1,1,1,0,0,1,0,0,1],
+    [1,1,1,1,0,0,0,1,0,0,0,0,0,0,1,0,0,1,1,1,1],
+    [1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,1,1,1,1],
+    [1,0,0,1,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,1],
+    [1,0,0,1,0,0,0,1,0,0,0,0,0,0,1,0,0,1,0,0,1],
+    [1,1,1,1,1,0,0,1,0,0,0,0,0,0,1,0,0,1,1,1,1],
+    [1,0,0,0,1,0,0,1,1,1,0,0,1,1,1,0,0,0,0,0,1],
     [1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,1,0,0,0,0,0,0,0,1,1,1,1,1,1,0,0,1],
+    [1,0,0,0,1,0,0,0,0,0,0,1,1,1,1,1,1,1,0,0,1],
     [1,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1],
     [1,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1],
     [1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1],
@@ -34,29 +34,39 @@ default_pnj_path = './assets/game/pnj/default-pnj.png'
 epstein_pnj_path = './assets/game/pnj/epstein-pnj.png'
 arnaud_pnj_path = './assets/game/pnj/arnaud-pnj.png'
 zombie_path = './assets/game/pnj/zombie-pnj.png'
+bars_path = './assets/textures/bars.png'
 
 MAP_SPRITES_MOCKED = [
-    HumanSprite(1.5, 2, default_pnj_path, [
-        'Hello'
+    HumanSprite(1.5, 2, arnaud_pnj_path, [
+        'Salut à toi, tourne les flèches directionneles',
+        'Je suis le formateur je vais te donner des conseils'
     ]),
-    HumanSprite(2.5, 2, epstein_pnj_path, [
-        'Hello'
+    HumanSprite(19.5, 2, epstein_pnj_path, [
+        "OHHHH merci, je te suis reconnaissant de l'avoir ouvert",
+        "Un idiot m'a enfèrmé en prenant la clé de ma cellule"
     ]),
-    HumanSprite(3.5, 2, arnaud_pnj_path, [
-        'Hello'
+    HumanSprite(11.5, 1.5, default_pnj_path, [
+        "Un mec fou passe son temps à taper dans sa cellule",
+        "Il mérite d'etre enfèrmé selon moi",
+        "j'ai caché sa clé il ne sortira pas pour l'instant"
     ]),
-    DoorSprite(6, 1.5, arnaud_pnj_path),
-    DoorSprite(8, 1.5, arnaud_pnj_path),
-    EnemieSprite(5, 1.5, zombie_path, 50, 20),
-    ObjectSprite(1.5, 3, ItemFactory.vodka()),
-    ObjectSprite(2.5, 3, ItemFactory.code(1)),
-    ObjectSprite(3.5, 3, ItemFactory.code(2)),
-    ObjectSprite(4.0, 3, ItemFactory.key()),
-    ObjectSprite(4.5, 3, ItemFactory.key()),
-    ObjectSprite(1.5, 4, ItemFactory.canned()),
-    ObjectSprite(2.5, 4, ItemFactory.gun()),
-    ObjectSprite(3.5, 4, ItemFactory.ammo()),
-    ObjectSprite(4.5, 4, ItemFactory.ammo())
+    DoorSprite(19.5, 4,bars_path ),
+    DoorSprite(16.5, 16, bars_path ),
+    DoorSprite(17.5, 6.5, bars_path),
+    EnemieSprite(8, 17, zombie_path, 50, 20),
+    ObjectSprite(2.5,7, ItemFactory.vodka()),
+    ObjectSprite(19.5, 1.5, ItemFactory.code(1)),
+    ObjectSprite(19,7.5 , ItemFactory.code(2)),  
+    ObjectSprite(18.2, 12.3, ItemFactory.code(3)),
+    ObjectSprite(3.5, 16.5, ItemFactory.code(4)),
+    ObjectSprite(16.5, 18.5, ItemFactory.code(5)),
+    ObjectSprite(2.5,3.5, ItemFactory.key()),
+    ObjectSprite(19.5,3 , ItemFactory.key()),
+    ObjectSprite(18.5,7.5,ItemFactory.key()),
+    ObjectSprite(1.5, 2.5, ItemFactory.canned()), 
+    ObjectSprite(13.3, 9.5, ItemFactory.gun()),
+    ObjectSprite(10, 9.5, ItemFactory.ammo()),
+    ObjectSprite(10.5, 9.5, ItemFactory.ammo())
 ]
 
 MAP_TEXTURES_MOCKED = {
