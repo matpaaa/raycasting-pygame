b5b033845f6312a06d53d7cafa9c2c98d1d68f5e
EGOUCHOLA <egoucholamalick229@gmail.com>
2026-04-25 17:37:25 +0100

    mis a jours des models et des api que j'avais eu a faire

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import account,map,save
+from .models import account,map,save,player
 # Register your models here.
 admin.site.register(account)
 admin.site.register(map)
 admin.site.register(save)
+admin.site.register(player)
